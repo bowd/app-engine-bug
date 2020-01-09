5c53750e8d1e975560a880e4f0a50826b3279aeb
Bogdan Dumitru <dumbogdan@gmail.com>
2020-01-09 13:15:55 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import google.cloud.logging
+
+client = google.cloud.logging.Client()
+client.setup_logging()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    app.logger.info("This is a test")
+    return "Hello World!"
